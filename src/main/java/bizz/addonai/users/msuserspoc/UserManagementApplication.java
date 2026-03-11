81929d70029cac81303cdb08bd57cafc8e10511a
Jiliar Silgado <jiliar.silgado@gmail.com>
2026-03-10 20:19:35 -0500

    chore(debug): add trace logging for spring web

--- src/main/java/bizz/addonai/users/msuserspoc/UserManagementApplication.java
+++ src/main/java/bizz/addonai/users/msuserspoc/UserManagementApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
-@ComponentScan(basePackages = "bizz.addonai.users.msuserspoc")
 public class UserManagementApplication {
 
     public static void main(String[] args) {
